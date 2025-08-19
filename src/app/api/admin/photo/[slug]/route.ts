0c6f7bf0b66cbc0eb0df37332eb36afb669d0a52
gitneohawk <neohawk@evoluzio.com>
2025-08-19 16:59:06 +0900

    some file modified to import from auth.ts

--- src/app/api/admin/photo/[slug]/route.ts
+++ src/app/api/admin/photo/[slug]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { BlobServiceClient } from "@azure/storage-blob";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
--- src/app/api/admin/photos/route.ts
+++ src/app/api/admin/photos/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import type { Prisma } from "@prisma/client";
 
 export const runtime = "nodejs";
