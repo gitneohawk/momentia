1ef8a6db97de3e8d754cb8ff28ffd06d9f66f226
gitneohawk <neohawk@evoluzio.com>
2025-09-24 17:51:01 +0900

    prisma.config.js deleted

--- prisma.config.js
+++
@@ -1,7 +0,0 @@
-// prisma.config.js
-module.exports = {
-  schema: './prisma/schema.prisma',
-  dotenv: '.env', // ここで .env を読ませる（必要なら）
-  // 追加設定があればここに追記できます
-  // migrations: { path: './prisma/migrations' },
-};
